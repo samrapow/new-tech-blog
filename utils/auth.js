9f264d175aeb25c080d55bd338518ef2f25ffc25
Sam Rapowitz <samrapow@gmail.com>
2022-02-11 23:23:55 -0500

    created main.handlebars layout in views

--- utils/auth.js
+++ utils/auth.js
@@ -3,8 +3,6 @@ const withAuth = (req, res, next) => {
     if (!req.session.loggedIn) {
       res.redirect('/login');
     } else {
-      // If the user is logged in, execute the route function that will allow them to view the page
-      // We call next() if the user is authenticated
       next();
     }
   };
